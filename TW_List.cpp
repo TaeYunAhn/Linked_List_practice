d5ef7da1650bfd4ecfa814a0ec5cfa7535eba071
TaeYunAhn <80814896+TaeYunAhn@users.noreply.github.com>
2021-06-09 14:32:03 +0900

    21.06.09

--- TW_List.cpp
+++ TW_List.cpp
@@ -11,9 +11,13 @@ TW_List::TW_List()
 
 TW_List::~TW_List()
 {
-    delete head;
-    delete tail;
-    
+    Node* cur_node = head;
+    while ( cur_node != nullptr )
+    {
+        Node* del_node = cur_node;
+        cur_node = cur_node->next;
+        delete del_node;
+    }
 }
 
 bool TW_List::append(std::string data)
@@ -98,8 +102,8 @@ bool TW_List::del(int index)
 
     //지우려는 노드 전 노드랑 지우는 노드 다음 노드 연결
     // 해당 노드 지우기.
-    Node *before_node = new Node;
-    Node *cur_node = new Node;
+    Node *before_node = head;
+    Node *cur_node = head;
     cur_node = cur_node->next;
 
     int count = 1;
@@ -107,20 +111,32 @@ bool TW_List::del(int index)
     {
         if ( index == 0 )
         {
-            cur_node->next = head;
-            delete cur_node;
-            return true;
+            if (head == tail )
+            {
+                delete cur_node;
+                return true;
+            }
+            else
+            {
+                head = before_node->next;
+                delete before_node;
+                return true;
+            }
         }
+
         if ( index == count )
         {
             before_node->next = cur_node->next;
+            if ( cur_node == tail )
+            {
+                tail = before_node;
+            }
+
             delete cur_node;
             return true;
         }
-       /* if ( (index )
-        {
+        
 
-        }*/
         count++;
         before_node = before_node->next;
         cur_node = cur_node->next;
@@ -134,20 +150,23 @@ Node *TW_List::at(int index)
     if ( index < 0 )
         return nullptr;
 
-    Node *cur_node = new Node;
+    Node *cur_node = head;
 
     int count = 0;
-    while ( index == count);
+    while ( index == count)
     {
         return cur_node;
+
+        count++;
+        cur_node = cur_node->next;
     }
-    count++;
-    cur_node = cur_node->next;
+
+    return nullptr;
 }
 
 Node *TW_List::find(std::string data)
 {
-    Node *cur_node = new Node;
+    Node *cur_node = head;
     int count = 0;
     std::string find_what;
 
@@ -160,6 +179,8 @@ Node *TW_List::find(std::string data)
         count++;
         cur_node = cur_node->next;
     }
+
+    return nullptr;
 }
 
 bool TW_List::print() const
